9c48ee0bdece2f2868a02bb379c7167fa6ca2747
kommu <kommu@LAPTOP-NQT59RFB>
2025-07-25 07:58:47 +0530

    In this commit, we have learned serialization and deserialization using POJO classes. And also OAuth authentication in Rest API. All POJO classes are under pojoClassesDemos package

--- src/main/java/OAuth_demo.java
+++ src/main/java/OAuth_demo.java
@@ -2,10 +2,15 @@
 
 import static io.restassured.RestAssured.*;
 
+import java.util.List;
+
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import payload.ReusableMethods;
+import pojoClassesDemos.Api;
+import pojoClassesDemos.GetCourses;
+import pojoClassesDemos.WebAutomation;
 
 public class OAuth_demo {
 
@@ -17,13 +22,32 @@ public class OAuth_demo {
 		formParam("grant_type", "client_credentials").
 		formParam("scope", "trust").when().post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
 		
-		System.out.println(response1);
+//		System.out.println(response1);
 		
 		JsonPath js = ReusableMethods.rawToJson(response1);
 		String accessToken = js.getString("access_token");
-		String response2 = given().queryParam("access_token", accessToken).when().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+		GetCourses response2 = given().queryParam("access_token", accessToken).when().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourses.class);
 		
-		System.out.println(response2);
+//		System.out.println(response2);
+		System.out.println(response2.getLinkedIn());
+		System.out.println(response2.getInstructor()); 
+		System.out.println("Finding course title using index");
+		System.out.println(response2.getCourses().getApi().get(1).getCourseTitle());
+		System.out.println("Finding course title (dynamical element) to get price of cource");
+		List<Api> apiCourses = response2.getCourses().getApi();
+		for(int i=0; i<apiCourses.size(); i++) {
+			if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")) {
+				System.out.println(apiCourses.get(i).getPrice());
+			}
+		}
+		
+		System.out.println("Get me the all cource titles of WebAutomation");
+		List<WebAutomation> WebAutomationCourses = response2.getCourses().getWebAutomation();
+		for(int i=0; i<WebAutomationCourses.size(); i++) {
+			System.out.println(WebAutomationCourses.get(i).getCourseTitle());
+		}
+//		String WebAutomationCourses = response2.getCourses().getWebAutomation().get(1).getCourseTitle();
+		
 
 	}
 
---
+++ src/main/java/pojoClassesDemos/GetCourses.java
@@ -0,0 +1,52 @@
+package pojoClassesDemos;
+
+public class GetCourses {
+
+	private String url;
+	private String services;
+	private String expertise;
+	private courses Courses;
+	private String instructor;
+	private String LinkedIn;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getServices() {
+		return services;
+	}
+	public void setServices(String services) {
+		this.services = services;
+	}
+	public String getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(String expertise) {
+		this.expertise = expertise;
+	}
+	public courses getCourses() {
+		return Courses;
+	}
+	public void setCourses(courses courses) {
+		Courses = courses;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public String getLinkedIn() {
+		return LinkedIn;
+	}
+	public void setLinkedIn(String linkedIn) {
+		LinkedIn = linkedIn;
+	}
+	
+	
+	
+	
+}
